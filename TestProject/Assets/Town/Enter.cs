2522d9273ac073f3841e24be1ef1b478f139c773
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-05-18 22:54:35 +0100

    Can close town ui
    Started on loading in different town names and such so that's what I
    want to continue on.

--- TestProject/Assets/Town/Enter.cs
+++ TestProject/Assets/Town/Enter.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class Enter : MonoBehaviour 
 {
 	public GameObject townUi;
+	public int[] goods = new int[10];
 	public string townname = "default";
 	public string subtitle = "default";
 	public string descLocation = "default";
-	// You can break free from this by using the normal pause button. Though I'll have to get rid of that later so you can't drive off with the town menu open.
 
 	// Basic idea here is to generate a gui with all the town details on from the information above.
 	void OnTriggerStay2D(Collider2D other)
@@ -19,7 +19,7 @@ public class Enter : MonoBehaviour
 				Time.timeScale = 0;
 				townUi.SetActive (true);
 				LoadTown town = (LoadTown)townUi.GetComponent(typeof(LoadTown));
-				town.load(townname, subtitle, descLocation);
+				town.load(townname, subtitle, descLocation, goods);
 
 				// for each goods in good that doesnt have an element of value -1 put a new good in and set it's price to whatever
 			} 
--- TestProject/Assets/Town/LoadTown.cs
+++ TestProject/Assets/Town/LoadTown.cs
@@ -1,17 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LoadTown : MonoBehaviour 
 {
-	public int[] goods = new int[10];
+	private int[] goods = new int[10];
+	public GameObject title;
+	public GameObject subtitle;
+	public GameObject descriptionBox;
 
-	public void load(string name, string subtitle, string descLocation)
+	public void load(string name, string subtitle, string descLocation, int[] goods)
 	{
+		title.GetComponent<Text>() = name;
+		subtitle.GetComponent<Text>() = subtitle;
+		// read in description via descLocation
+		string description = "default";
+		descriptionBox.GetComponent<Text>() = description; 
+	}
 
+	public void undock()
+	{
+		Time.timeScale = 1;
+		gameObject.SetActive (false);
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
 }
