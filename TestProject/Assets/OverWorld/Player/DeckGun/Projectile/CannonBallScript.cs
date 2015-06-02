05e2a9f300e796f33abcea0b2b90c237e1c85e5f
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-06-02 22:16:35 +0100

    Cannonballs spawn a little better
    It's still extremely janky though

--- TestProject/Assets/OverWorld/Player/DeckGun/Projectile/CannonBallScript.cs
+++ TestProject/Assets/OverWorld/Player/DeckGun/Projectile/CannonBallScript.cs
@@ -4,17 +4,19 @@ using System.Collections;
 public class CannonBallScript : MonoBehaviour 
 {	
 	private GameObject deckGun;
-	private Transform position;
-	public float speed = 100f;
-	private float lifetime = 80f;	
-
+	private Transform myTransform;
+	private float speed = 50f;
+	private float lifetime = 120f;	
+	// Spawns at the front of the gun with the correct rotation, There's definitely a better way to do this, I just need to get around to it.
+	// First idea is to create an empty to be hte casting point and have that handle the event rather than the gun.
 	void Start () 
 	{
 		deckGun = GameObject.FindWithTag("gun");
-		position = deckGun.transform;
-		gameObject.transform.rotation = position.rotation;
-		gameObject.transform.position = position.position;
-		gameObject.transform.position += Vector3.forward * 10f;
+		myTransform = deckGun.transform;
+		gameObject.transform.rotation = myTransform.rotation;
+		gameObject.transform.position = myTransform.position;
+		gameObject.transform.position += Vector3.forward * 0f;
+		gameObject.transform.position = new Vector3 (transform.position.x, transform.position.y, -5f);
 	}
 
 	void FixedUpdate () 
